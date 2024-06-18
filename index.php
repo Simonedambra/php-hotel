d0ed11fcd41f06f6e0b794040dc39716b8591f23
Simonedambra <simonedambra130102@gmail.com>
2024-06-18 17:14:50 +0200

    [upd] table

--- index.php
+++ index.php
@@ -40,12 +40,81 @@ $hotels = [
 
 ];
 
-foreach ($hotels as $hotellist) {
+// foreach ($hotels as $hotellist) {
 
-    foreach ($hotellist as $key => $hotel) {
-        echo $key;
-        echo ' ';
-        echo $hotel;
-        echo '<br>';
+//     foreach ($hotellist as $key => $hotel) {
+//         echo $key;
+//         echo ' ';
+//         echo $hotel;
+//         echo '<br>';
+//     }
+// }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+
+    <table class="table">
+        <thead>
+
+
+            <tr>
+                <?php foreach ($hotels as $hotellist) {
+                    echo '<th scope="row">';
+                    foreach ($hotellist as $key => $value) {
+                        echo $value;
+                    }
+                    echo '</th>';
+                }
+
+
+
+                ?>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+    <hr>
+    <?php foreach ($hotels as $hotellist) {
+        echo '<th scope="row">';
+        foreach ($hotellist as $key => $value) {
+            echo $value;
+
+            echo '<br>';
+        }
+        echo '</th>';
     }
-}
+
+
+
+    ?>
+</body>
+
+</html>
